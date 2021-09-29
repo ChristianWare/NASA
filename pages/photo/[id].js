166be68fbe1b44b5641100f0487507f464a38578
Chris <chris.ware.dev@gmail.com>
2021-09-29 14:52:22 -0700

    Image Preview Page and Slug GTP's

---
+++ pages/photo/[id].js
@@ -0,0 +1,15 @@
+import {useRouter} from 'next/router'
+import Image from 'next/image'
+import Link from 'next/link'
+
+function photo({ photo }) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+
+export default photo
